27401f55cbf63810ec64f72b64561448c19af986
blckmn <blackman@xtra.com.au>
2019-04-28 20:02:24 +1000

    Adding cloudfront cache invalidation as part of the release script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,4 +22,5 @@ node ${basepath}/node_modules/nanogen/lib/cli build
 
 if [ "${1}" == "deploy" ]; then
   aws s3 sync ${artifacts} s3://${BUCKET} --delete --region "${REGION}"
+  aws cloudfront create-invalidation --distribution-id ${AWS_DISTRIBUTION_ID} --path /*
 fi
