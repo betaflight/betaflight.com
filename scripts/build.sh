980e662faa902d3fd22f5f30a2374a4d9b58de5f
blckmn <blackman@xtra.com.au>
2019-05-04 16:41:19 +1000

    Including cache-control setting.

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,6 +21,6 @@ node --version
 node ${basepath}/node_modules/nanogen/lib/cli build
 
 if [ "${1}" == "deploy" ]; then
-  aws s3 sync ${artifacts} s3://${BUCKET} --delete --region "${REGION}"
+  aws s3 sync ${artifacts} s3://${BUCKET} --delete --region "${REGION}" --cache-control max-age=31104000
   aws cloudfront create-invalidation --distribution-id ${AWS_DISTRIBUTION_ID} --path /*
 fi
