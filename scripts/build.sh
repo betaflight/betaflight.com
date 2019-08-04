2876871284fa750600663de9db38a42f42998a6d
mikeller <github@ike.ch>
2019-08-04 13:33:40 +1200

    Second attempt at fixing the AWS auto deploy.

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,6 +21,7 @@ node --version
 node ${basepath}/node_modules/nanogen/lib/cli build
 
 if [ "${1}" == "deploy" ]; then
+  aws configure set preview.cloudfront true
   aws s3 sync ${artifacts} s3://${BUCKET} --delete --region "${REGION}" --cache-control max-age=31104000
   aws cloudfront create-invalidation --distribution-id ${AWS_DISTRIBUTION_ID} --path /*
 fi
