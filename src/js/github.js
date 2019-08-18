560d23b3480df1bcc3995327c0b63b7c7bcb8382
blckmn <blackman@xtra.com.au>
2019-08-18 19:32:32 +1000

    Better naming of config for axios.

--- src/js/github.js
+++ src/js/github.js
@@ -3,7 +3,7 @@ const logger = require('./logger');
 
 const baseUrl = 'https://api.github.com';
 
-const config = {
+const axiosConfig = {
     auth: {
         username: process.env.GITHUB_LOGIN,
         password: process.env.GITHUB_TOKEN
@@ -12,7 +12,7 @@ const config = {
 
 const getMember = async (memberLogin) => {
     logger.info(` - getting member: ${memberLogin}`)
-    const response = await axios.get(`${baseUrl}/users/${memberLogin}`, config);
+    const response = await axios.get(`${baseUrl}/users/${memberLogin}`, axiosConfig);
     logger.success(`  -> ${response.data.login}`);
     return response.data;
 }
@@ -20,7 +20,7 @@ const getMember = async (memberLogin) => {
 const getMembers = async (org) => {
 
     logger.info('Getting public members:');
-    const response = await axios.get(`${baseUrl}/orgs/${org}/public_members`, config);
+    const response = await axios.get(`${baseUrl}/orgs/${org}/public_members`, axiosConfig);
 
     var memberLogins = response.data.map(member => member.login);
     logger.success(memberLogins);
