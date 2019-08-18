f17a223aca00c8f7d255a7e9ca07387fa7e3fc2d
blckmn <blackman@xtra.com.au>
2019-08-18 19:55:05 +1000

    More logging.

--- src/js/build.js
+++ src/js/build.js
@@ -15,12 +15,13 @@ const prepare = async () => {
     logger.success(`Using path: ${srcPath}`);
     config.build.srcPath = srcPath;
 
-    logger.info('Getting member data:');
+    logger.info('*** Getting member data ***');
     config.site.members = await gh.getMembers('betaflight');
-
     logger.info('----');
-    logger.info('Getting page data:');
+
+    logger.info('*** Getting page data ***');
     config.site.news = pageUtil.getPageData(pagesPath, 'article').sort((a, b) => a.date > b.date ? -1 : 1);;
+    logger.info('----');
 };
 
 prepare().then(() => {
