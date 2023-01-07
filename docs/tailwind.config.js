88fd927840a311e19fdccf77737a623c9efc9bac
VitroidFPV <vitroid05@gmail.com>
2023-01-08 00:04:58 +0100

    First steps toward proper theming

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					200: "#FFDA66",
+					300: "#FFD044",
+					400: "#FFC622",
+					500: "#FFBB00",
+					600: "#EEA600",
+					700: "#DD9100",
+					800: "#CC7E00",
+				}
+			},
+			backgroundSize: {
+				"0w": "0% 3px",
+				"100w": "100% 3px"
+			},
+		},
+	},
+	plugins: [],
 }
