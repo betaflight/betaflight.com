4c9449b23dacd2c8aae4cfbdb7214c6b24fb3fff
Eike Ahmels <eike.ahmels@tu-dortmund.de>
2023-01-08 11:08:19 +0100

    enforce dark mode, remove switch

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -81,6 +81,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
       navbar: {
         title: 'Betaflight',
         logo: {
