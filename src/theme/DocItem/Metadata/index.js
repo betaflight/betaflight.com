bcf9a03853f689a5047ca39b52597a5f5c55b2b7
VitroidFPV <vitroid05@gmail.com>
2023-02-08 23:05:05 +0100

    Added semicolons back from eslint misconfig

--- src/theme/DocItem/Metadata/index.js
+++ src/theme/DocItem/Metadata/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { PageMetadata } from '@docusaurus/theme-common'
-import { useDoc } from '@docusaurus/theme-common/internal'
+import React from 'react';
+import { PageMetadata } from '@docusaurus/theme-common';
+import { useDoc } from '@docusaurus/theme-common/internal';
 export default function DocItemMetadata() {
-  const { metadata, frontMatter } = useDoc()
+  const { metadata, frontMatter } = useDoc();
   return (
     <PageMetadata
       title={metadata.title}
@@ -10,5 +10,5 @@ export default function DocItemMetadata() {
       keywords={frontMatter.keywords}
       image={`https://vitroidfpv.com/og-bf?title=${metadata.title}&description=${metadata.description}`}
     />
-  )
+  );
 }
