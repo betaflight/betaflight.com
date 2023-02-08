39cca4d8a3762eb120cd053455574634fc02f93c
VitroidFPV <vitroid05@gmail.com>
2023-02-08 21:44:26 +0100

    Fixed SonarCloud code smell check

--- src/theme/DocItem/Metadata/index.js
+++ src/theme/DocItem/Metadata/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { PageMetadata } from '@docusaurus/theme-common'
 import { useDoc } from '@docusaurus/theme-common/internal'
 export default function DocItemMetadata() {
-  const { metadata, frontMatter, assets } = useDoc()
+  const { metadata, frontMatter } = useDoc()
   return (
     <PageMetadata
       title={metadata.title}
