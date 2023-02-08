aff9acbcb89c120bf7545b562a8eeaea4b4c703e
VitroidFPV <vitroid05@gmail.com>
2023-02-08 20:47:25 +0100

    Added custom image embeds for docs

---
+++ src/theme/DocItem/Metadata/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { PageMetadata } from '@docusaurus/theme-common'
+import { useDoc } from '@docusaurus/theme-common/internal'
+export default function DocItemMetadata() {
+  const { metadata, frontMatter, assets } = useDoc()
+  return (
+    <PageMetadata
+      title={metadata.title}
+      description={metadata.description}
+      keywords={frontMatter.keywords}
+      image={`https://vitroidfpv.com/og-bf?title=${metadata.title}&description=${metadata.description}`}
+    />
+  )
+}
