1df229a260aec30b88794e68fd96b4458c67e3ee
Supafly FPV <steve@supaflyfpv.com>
2023-03-15 01:03:02 +0000

    Homepage edit

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,40 +1,40 @@
-import React from 'react'
-import { CameraIcon, CodeBracketIcon, CpuChipIcon, DocumentTextIcon, ShieldCheckIcon, UsersIcon } from '@heroicons/react/24/solid'
-import JetIcon from '@site/src/icons/jet.icon.svg'
-import { BlogProps } from '@site/src/types'
-import BetaflightLayout from '../Layout'
-import HomepageFeature from '../HomepageFeature'
-import AboutCard from '../AboutCard'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPatreon, faPaypal } from '@fortawesome/free-brands-svg-icons'
-import Button from '../Button'
-import TeamFeature from '../Team'
+import React from 'react';
+import { CameraIcon, CodeBracketIcon, CpuChipIcon, DocumentTextIcon, ShieldCheckIcon, UsersIcon } from '@heroicons/react/24/solid';
+import JetIcon from '@site/src/icons/jet.icon.svg';
+import { BlogProps } from '@site/src/types';
+import BetaflightLayout from '../Layout';
+import HomepageFeature from '../HomepageFeature';
+import AboutCard from '../AboutCard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPatreon, faPaypal } from '@fortawesome/free-brands-svg-icons';
+import Button from '../Button';
+import TeamFeature from '../Team';
 
 export default function Home({ recentPosts }: BlogProps) {
   function clampAndFormatMinutes(minutes: number) {
     if (minutes < 1) {
-      return 'One minute read'
+      return 'One minute read';
     }
-    return `${minutes} minutes read`
+    return `${minutes} minutes read`;
   }
 
   function formatDate(date: string) {
-    const dateObj = new Date(date)
+    const dateObj = new Date(date);
     return dateObj.toLocaleDateString('en-US', {
       year: 'numeric',
       month: 'long',
       day: '2-digit',
-    })
+    });
   }
 
   function onClickDonate(service: string) {
     switch (service) {
       case 'paypal':
-        return () => window.open('https://paypal.me/betaflight', '_blank', 'noopener')
+        return () => window.open('https://paypal.me/betaflight', '_blank', 'noopener');
       case 'patreon':
-        return () => window.open('https://patreon.com/betaflight', '_blank', 'noopener')
+        return () => window.open('https://patreon.com/betaflight', '_blank', 'noopener');
       default:
-        break
+        break;
     }
   }
 
@@ -44,12 +44,16 @@ export default function Home({ recentPosts }: BlogProps) {
         <div className="w-full h-fit flex flex-col justify-center md:justify-start">
           <div className="flex flex-col p-6 h-fit w-fit xl:ml-12">
             <h1 className="md:text-[6rem] text-6xl border-primary-500 font-bold mb-4">Betaflight</h1>
-            <h2 className="text-white font-semibold md:text-3xl text-xl">Are you ready to fly?</h2>
+            <h2 className="text-white font-semibold md:text-3xl text-xl">Pushing the Limits of UAV Performance</h2>
           </div>
           <div className="p-4 xl:p-16 flex w-full justify-around flex-col xl:flex-row space-y-4 xl:space-y-0 space-x-0 xl:space-x-16">
             <div className="backdrop-blur-md shadow-xl w-full xl:w-1/2 flex xl:self-start self-center text-lg p-4 rounded-2xl bg-neutral-500/10">
-              Betaflight is flight controller software (firmware) used to fly multi-rotor craft and fixed wing craft. This fork differs from Baseflight and Cleanflight in that it focuses on flight
-              performance, leading-edge feature additions, and wide target support.
+              Betaflight is a flight firmware used for controlling multi-rotor radio controlled aircraft. <br></br>
+              <br></br>
+              Originating from the Baseflight and Cleanflight open source projects, the project was branched off as a high performance 'Beta' testbed. <br></br>
+              <br></br>
+              Now matured with years of evidence lead development, Betaflight has grown into the largest flight firmware userbase in the sector due to its cutting edge features, performance and
+              reliability.
             </div>
             <div className="flex-grow w-full xl:w-1/2">
               <AboutCard title="Recent posts" className="text-primary-500" Icon={DocumentTextIcon}>
@@ -78,43 +82,29 @@ export default function Home({ recentPosts }: BlogProps) {
         <HomepageFeature title="About" compact={true}>
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 xl:gap-6 w-full">
             <AboutCard title="Hardware" className="text-rose-500 fill-rose-500" Icon={CpuChipIcon}>
-              <p>Betaflight supports a wide range of flight controllers with at least an STM32F4 MCU</p>
-              <p>You can use nearly any piece of modern hardware out there. Full telemetry and high refresh rate for receivers, camera and VTX control, RGB LEDs, and much more!</p>
-              <p>Run the configurator on any device, whether it is using Windows, MacOS, Linux, or Android</p>
+              <p>Betaflight supports a wide range of flight controllers from a variety of manufacturers.</p>
+              <p>The 'Approved Partner' program provides hardware recommendations from the development team based.</p>
             </AboutCard>
 
             <AboutCard title="Community" className="text-sky-500 fill-sky-500" Icon={UsersIcon}>
-              <p>Our community is very active and helpful. We have a very active Discord server with over 4,000 members, so you can get help if you're stuck on something</p>
-              <p>You can also find a ton of helpful info on youtube, and this very site</p>
+              <p>The user community is active and helpful, with a Facebook group of over 30,000 members and growing Discord server.</p>
+              <p>Considerable support resources also exist on youtube made by third parties.</p>
             </AboutCard>
 
             <AboutCard title="Open Source" className="text-lime-500 fill-lime-500" Icon={CodeBracketIcon}>
-              <p>Originating from Baseflight and Cleanflight, Betaflight is open source, and you can contribute to the project on GitHub</p>
-              <p>Nearly all contributions are welcome, for anything from the main flight control firmware, to the configurator, as well as this site</p>
+              <p>Betaflight is 'Open Source' code, so you can view and contribute to the project on GitHub.</p>
+              <p>The team has a robust review system in order to maintain clean code, and we are always looking for talented contributors.</p>
             </AboutCard>
             <AboutCard className="border-blue-500 text-blue-500" title="OSD" Icon={CameraIcon}>
-              <p>
-                With the Betaflight OSD you can get all relevant flight metrics directly into your FPV video feed. An easy to use drag-and-drop configuration allows the placement of values like used
-                mAh and LiPo Voltage readings.
-              </p>
-              <p>Additionally you can change most firmware settings using stick commands without even removing your goggles.</p>
+              <p>With the Betaflight On Screen Display you can use drag-and-drop to set up key flight metrics into your FPV video feed. </p>
+              <p> This allows data such as battery metrics, speed, altitude and home direction.</p>
             </AboutCard>
             <AboutCard className="border-orange-600 text-orange-600" title="Safety Features" Icon={ShieldCheckIcon}>
-              <p>
-                Drones are extremely dangerous toys. As incidents with suddenly spinning props often end with serious injury, Betaflight implements several safety features to prevent dangerous
-                situations occurring.
-              </p>
-              <p>Features are implemented to prevent arming when the drone isn't leveled, including disabling motors in case of wrong motor or incorrect flight controller orientation.</p>
+              <p>Alerts for and arming blocks for improper setup, and disarm mechanisms are built in to avoid accidents. </p>
+              <p>A comprehensive failsafe mechanism is featured to assist in flight issues</p>
             </AboutCard>
             <AboutCard className="border-purple-600 text-purple-600" title="Flight Dynamics" Icon={JetIcon}>
-              <p>
-                Betaflight not only strives to put the best flight code on your controller, it also looks to satisfy such requirements as perfect performance, tiny footprint, full feature set and a
-                strong quality assurance process.
-              </p>
-              <p>
-                To assist in this endeavour all flight related data can be logged into a blackbox for later analysis. So pilots and technicians can objectively base their tune on the most appropriate
-                data.
-              </p>
+              <p>Betaflight was created for cutting edge flight performance. This has been achieved optimising reaction time to disturbances, and increasing the accuracy of stick tracking.</p>
             </AboutCard>
           </div>
         </HomepageFeature>
@@ -124,11 +114,10 @@ export default function Home({ recentPosts }: BlogProps) {
         <HomepageFeature title="Donating">
           <div className="flex flex-col xl:flex-row space-x-4 justify-center items-center">
             <div className="flex flex-col space-y-4 text-white">
-              <p>Many of our users don't have the time to contribute but do love our software such that they want to continue to support us. The best way is to help out by donating.</p>
-              <p>
-                What are donations used for? We use the donations to help with the maintainers out of pocket costs, e.g. running build servers etc. We also use it to purchase test equipment, e.g.
-                Saleae logic analysers, and other items to assist consistent contributors in their efforts. We are exceptionally grateful for those who do donate, either their time or funds.
-              </p>
+              <p>Highly skilled developers lend their time for free in order to develop and maintain this project.</p>
+              <p>We would really appreciate your support if you enjoy this firmware or use it for professional work.</p>
+              <p>We use the donations to help with out of pocket costs, e.g. test hardware, hardware analysis equipment and running build servers.</p>
+              <p>We are exceptionally grateful for those who do donate, either their time or funds.</p>
             </div>
             <div className="md:flex gap-4 mt-4">
               <div className="md:w-60 w-full text-center rounded-2xl bg-neutral-500/10 p-6 flex flex-col justify-between">
@@ -144,7 +133,7 @@ export default function Home({ recentPosts }: BlogProps) {
                   <FontAwesomeIcon icon={faPatreon} className="text-red-600 mr-2" />
                   Patreon
                 </h1>
-                <p className="my-4">To setup a monthly recurring donation for ongoing support and commitment.</p>
+                <p className="my-4">To setup a monthly recurring donation for ongoing support, commitment and thanks.</p>
                 <Button onClick={onClickDonate('patreon')}>Donate</Button>
               </div>
             </div>
@@ -152,5 +141,5 @@ export default function Home({ recentPosts }: BlogProps) {
         </HomepageFeature>
       </div>
     </BetaflightLayout>
-  )
+  );
 }
