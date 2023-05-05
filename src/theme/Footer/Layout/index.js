1e96855a93abb7ced68744fbff9e3e6ef604ba26
KarateBrot <Rm2k-Freak@web.de>
2023-05-05 11:12:49 +0200

    Less intrusive footer logo

--- src/theme/Footer/Layout/index.js
+++ src/theme/Footer/Layout/index.js
@@ -7,9 +7,9 @@ export default function FooterLayout({ style, links, logo, copyright }) {
       <div className="text-center">{links}</div>
       {copyright && (
         <div className="text-center">
-          <Logo className="w-40 h-40 mr-12 mt-4 mb-2 inline-block"></Logo>
+          <Logo className="w-36 h-36 mr-12 mt-4 inline-block opacity-10"></Logo>
           {copyright}
-          <div className="text-sm text-neutral-500/60">Built with Docusaurus</div>
+          <div className="mt-1 text-sm text-neutral-500/60">Built with Docusaurus</div>
           <div className="mt-1 text-sm text-neutral-500/60">
             made with <span className="text-red-500">❤️</span> by{' '}
             <a className="font-bold hover:text-emerald-500 no-underline duration-300" href="https://github.com/vitroidfpv">
