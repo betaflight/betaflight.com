a89ae22e012de10b379cc3f2d4043f66b57d93f1
VitroidFPV <vitroid05@gmail.com>
2023-05-10 20:19:38 +0200

    Content: Add Hardware page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
