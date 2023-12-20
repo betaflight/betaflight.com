6cd0da76de3de4616a21390aa6ae269deb7d7e6a
ctzsnooze <chris.thompson@sydney.edu.au>
2023-12-21 07:21:00 +1100

    note about colour formats

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // https://hslpicker.com/ to review colours easily
+        // colour review: https://hslpicker.com/
+        // colour format must be either:
+        // #XXXXXX, where characters must be uppercase, and all 6 values are required; or
+        // rgb (val, val, val) / rgb(val val val) (commas are not essential)
         primary: {
-          200: '#FFDA66', // pale orange, Hue 45, Luminance 70  #fd7 is Hue 45, Luminance 73
-          300: '#FFD044', // orange, Hue 45, Luminance 63  #fc3 is Hue 45, Luminance 60
-          400: '#FFC622', // orange, Hue 45, Luminance 57
-          500: '#FFBB00', // orange, Hue 44, Luminance 50 #fbo is Hue 45, Luminance 50.
-          600: '#EEA600', // orange, Hue 42, Luminance 47
-          700: '#DD9100', // orange, Hue 39, Luminance 43
-          800: '#CC7E00', // darker orange, Hue 37, Luminance 40 #c90 is Hue 45, Luminance 40
+          200: '#FFDA66', // pale orange,   Hue 45, Luminance 70
+          300: '#FFD044', // orange,        Hue 45, Luminance 63
+          400: '#FFC622', // orange,        Hue 45, Luminance 57
+          500: '#FFBB00', // mid orange,    Hue 44, Luminance 50
+          600: '#EEA600', // orange,        Hue 42, Luminance 47 ** coloured text **
+          700: '#DD9100', // orange,        Hue 39, Luminance 43
+          800: '#CC7E00', // browm,         Hue 37, Luminance 40
         },
         neutral: {
-          150: '#F0F0F0', // pale grey, Hue 0, Luminance 94; rgb(240, 240, 240)  #eee = Luminance 93
-          750: '#2C2C2C', // dark grey, Hue 0, Luminance 17, rgb(44, 44, 44) #222 + Luminance 13, #333 = luminance 20
+          150: '#F0F0F0', // rgb(240, 240, 240); pale grey, Hue 0, Luminance 94
+          750: '#2C2C2C', // rgb(44, 44, 44);    dark grey, Hue 0, Luminance 17
         },
       },
       backgroundSize: {
