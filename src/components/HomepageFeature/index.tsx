1d76170fbaf1072f304d4d21042f2ff3e50d3658
ctzsnooze <chris.thompson@sydney.edu.au>
2023-12-31 03:20:10 -0800

    subtle react changes for smaller screens

--- src/components/HomepageFeature/index.tsx
+++ src/components/HomepageFeature/index.tsx
@@ -17,9 +17,9 @@ export default function HomepageFeature({ title = '<unset>', compact = false, cl
     // </section>
     // make background blur depend on blur prop
     <section className={className}>
-      <h2 className="text-primary-600 text-3xl font-bold my-4 ml-7">{title}</h2>
+      <h2 className="text-primary-600 text-3xl font-bold m-0 sm:m-1 base:m-2 md:m-4">{title}</h2>
       {/* eslint-disable-next-line no-restricted-globals */}
-      <div className={clsx({ 'bg-neutral-500/10 shadow-xl p-8': !compact }, 'flex justify-center rounded-2xl', blur ? 'backdrop-blur-md' : '')}>{children}</div>
+      <div className={clsx({ 'bg-neutral-500/10 shadow-xl mb-6  p-4 xl:p-6': !compact }, 'flex justify-center rounded-2xl', blur ? 'backdrop-blur-md' : '')}>{children}</div>
     </section>
   );
 }
