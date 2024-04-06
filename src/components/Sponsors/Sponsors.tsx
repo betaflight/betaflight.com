0948546f19cb2273276779f3c678cc1ca9720bcc
Jan Post <Rm2k-Freak@web.de>
2024-04-06 23:14:20 +0200

    Fetch sponsor banner from BF server (#406)
    * Fetch sponsor banner from BF server
    
    * Change themeInvert to invertable
    
    * Use DOMPurify to sanitize response

---
+++ src/components/Sponsors/Sponsors.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import { useColorMode } from '@docusaurus/theme-common';
+import { sanitize } from 'dompurify';
+
+export default function Sponsors(): React.JSX.Element {
+  const [data, setData] = useState(null);
+  const { colorMode } = useColorMode();
+
+  async function fetchData(mode: string, page: string): Promise<void> {
+    const response = await fetch(`https://build.betaflight.com/api/configurator/sponsors/${mode}/${page}`);
+    setData(await response.text());
+  }
+
+  useEffect(() => {
+    fetchData(colorMode, 'landing');
+  });
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return <div dangerouslySetInnerHTML={{ __html: sanitize(data) }} />;
+}
