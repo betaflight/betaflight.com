24acc0a5cb41e776455121194210bc8b4998001e
KarateBrot <rm2k-freak@web.de>
2024-04-06 05:54:18 +0200

    Fetch sponsor banner from BF server

---
+++ src/components/Sponsors/Sponsors.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { useColorMode } from '@docusaurus/theme-common';
+
+export default function Sponsors(): React.JSX.Element {
+  const [data, setData] = useState(null);
+  const { colorMode } = useColorMode();
+
+  async function fetchData(mode: string, page: string): Promise<void> {
+    const response = await fetch(`https://build.betaflight.com/api/configurator/sponsors/${mode}/${page}`);
+    setData(await response.text());
+  }
+
+  useEffect(() => {
+    fetchData(colorMode, 'landing');
+  });
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return <div dangerouslySetInnerHTML={{ __html: data }} />;
+}
