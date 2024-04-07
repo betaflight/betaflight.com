8207ffd0a033c463c4a2df08175ba91da4762faa
KarateBrot <rm2k-freak@web.de>
2024-04-07 17:03:25 +0200

    Cache server response

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -1,23 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useColorMode } from '@docusaurus/theme-common';
 import { sanitize } from 'dompurify';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function Sponsors(): React.JSX.Element {
-  const [data, setData] = useState(null);
   const { colorMode } = useColorMode();
+  let data: string | null = localStorage.getItem(colorMode);
 
   async function fetchData(mode: string, page: string): Promise<void> {
-    const response = await fetch(`https://build.betaflight.com/api/configurator/sponsors/${mode}/${page}`);
-    setData(await response.text());
+    if (!data) {
+      const response = await fetch(`https://build.betaflight.com/api/configurator/sponsors/${mode}/${page}`);
+      data = await response.text();
+      localStorage.setItem(mode, data);
+    }
   }
 
   useEffect(() => {
     fetchData(colorMode, 'landing');
   });
 
-  if (!data) {
-    return <div>Loading...</div>;
-  }
+  return (
+    <BrowserOnly>
+      {() => {
+        if (!data) {
+          return <div>Loading...</div>;
+        }
 
-  return <div dangerouslySetInnerHTML={{ __html: sanitize(data) }} />;
+        return <div dangerouslySetInnerHTML={{ __html: sanitize(data) }} />;
+      }}
+    </BrowserOnly>
+  );
 }
