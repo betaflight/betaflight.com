02a7a2f1a3548cf99c1bcb9848a47f3dea349921
Eike Ahmels <ea@weslink.de>
2024-04-15 10:32:32 +0200

    suggested changes + transition effect

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -4,35 +4,38 @@ import { sanitize } from 'dompurify';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-const cycleInterval = 10000; // 10 seconds
+const cycleIntervalMs = 10000; // 10 seconds
 
 export default function Sponsors(): React.JSX.Element {
-  const [data, setData] = useState(null);
-
-  const [counter, setCounter] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, cycleInterval);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds((prevCounter) => prevCounter + 1);
-    }, cycleInterval / 5);
-
-    return () => clearInterval(interval);
-  }, []);
-
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<string | null>(null);
+  const [counter, setCounter] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
   const { colorMode } = useColorMode();
+
+  const countUp = (stateFunc: React.Dispatch<React.SetStateAction<number>>, intervalMs: number): (() => void) => {
+    const interval = setInterval(() => {
+      stateFunc((x) => x + 1);
+    }, intervalMs);
+
+    return () => clearInterval(interval);
+  };
+
+  useEffect(() => {
+    return countUp(setCounter, cycleIntervalMs);
+  }, []);
+
+  useEffect(() => {
+    return countUp(setSeconds, cycleIntervalMs / 5);
+  }, []);
 
   async function fetchData(mode: string, page: string): Promise<void> {
     const response = await fetch(`https://build.betaflight.com/api/configurator/sponsors/${mode}/${page}`);
-    setData(await response.text());
+    setLoading(true);
+    setTimeout(async () => {
+      setData(await response.text());
+      setLoading(false);
+    }, 1000);
   }
 
   useEffect(() => {
@@ -42,19 +45,19 @@ export default function Sponsors(): React.JSX.Element {
   return (
     <div>
       {data ? (
-        <div dangerouslySetInnerHTML={{ __html: sanitize(data) }} />
+        <div className={`transition-opacity duration-1000 min-h-[99px] opacity-0 ${loading ? '' : 'opacity-100'}`} dangerouslySetInnerHTML={{ __html: sanitize(data) }} />
       ) : (
-        <div className="text-2xl min-h-[180px] flex justify-center items-center">
+        <div className="min-h-[99px] text-2xl flex justify-center items-center">
           <FontAwesomeIcon className="mr-2" icon={faSpinner} spin />
           Loading...
         </div>
       )}
-      <div class="justify-center flex gap-4 mt-4">
+      <div className="justify-center flex gap-4 mt-4">
         {[...Array((seconds % 5) + 1)].map((_, i) => (
-          <div key={i} class="transition-all w-4 h-4 rounded-full bg-gray-500/20 border border-transparent" />
+          <div key={i} className="transition-all w-4 h-4 rounded-full bg-gray-500/20 border border-transparent" />
         ))}
         {[...Array(5 - (seconds % 5) - 1)].map((_, i) => (
-          <div key={i} class="w-4 h-4 rounded-full border bg-transparent border-gray-500/20" />
+          <div key={i} className="w-4 h-4 rounded-full border bg-transparent border-gray-500/20" />
         ))}
       </div>
     </div>
