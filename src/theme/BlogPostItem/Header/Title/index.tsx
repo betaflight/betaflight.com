1e02a5d60de8e5d2f8e13db826e101b22f69fe29
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2025-10-10 21:20:32 +0200

    Refactor: Swizzle Blog components and update styles

---
+++ src/theme/BlogPostItem/Header/Title/index.tsx
@@ -0,0 +1,14 @@
+import React, { type ReactNode } from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import { useBlogPost } from '@docusaurus/plugin-content-blog/client';
+import type { Props } from '@theme/BlogPostItem/Header/Title';
+
+import styles from './styles.module.css';
+
+export default function BlogPostItemHeaderTitle({ className }: Props): ReactNode {
+  const { metadata, isBlogPostPage } = useBlogPost();
+  const { permalink, title } = metadata;
+  const TitleHeading = isBlogPostPage ? 'h1' : 'h2';
+  return <TitleHeading className={clsx(styles.title, className, 'text-primary-500 font-bold')}>{isBlogPostPage ? title : <Link to={permalink}>{title}</Link>}</TitleHeading>;
+}
